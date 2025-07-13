8a876315c66a12a137f6d1c6d75e98d57da28c16
abhi1563 <abhijeet.kumar1563@gmail.com>
2025-07-13 19:57:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
-//create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to AWS with Abhi'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+  res.write('Welcome to AWS with Abhi');
+  res.end();
+}).listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
